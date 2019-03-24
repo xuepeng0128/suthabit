b1b4eba73127c82e100a9bdbaa0b38ec9ab3108d
yxpmatrix <2426574593@qq.com>
2019-03-24 20:52:03 +0800

    new project

---
+++ src/main/java/com/yxp/stuhabit/StuhabitApplication.java
@@ -0,0 +1,13 @@
+package com.yxp.stuhabit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuhabitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuhabitApplication.class, args);
+    }
+
+}
